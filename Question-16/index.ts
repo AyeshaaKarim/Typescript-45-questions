b4a59b9d9f817f4e04bc4cb4a1aa8d8efe273256
AyeshaKarim <139690499+AyeshaaKarim@users.noreply.github.com>
2023-09-23 22:31:01 +0500

    Add files via upload

---
+++ Question-16/index.ts
@@ -0,0 +1,42 @@
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+
+// Step 1: Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+let guestlist: Array<String> = [
+
+    "Ayesha Karim" , 
+    "Ayesha Sheikh" , 
+    "Arslan Ahmad" , 
+    "Zainub Karim" , 
+    "Fatima Nadeem"
+];
+
+ for (let guest of guestlist){
+    console.log(`Hey ${guest}, we have found a bigger dinner table.`)
+}; 
+
+// Step 2: Add one new guest to the beginning of your array.
+
+let newGuest: String = "Sehrish Khan";
+guestlist.unshift(newGuest);
+ console.log(guestlist);
+
+ // Step 3: Add one new guest to the middle of your array. 
+ // • Use append() to add one new guest to the end of your list. 
+ // • Print a new set of invitation messages, one for each person in your list.
+
+ let newGuestInMiddle: String = "Noor Mehmood";
+ let middle: number = Math.floor(guestlist.length/2)
+ guestlist.splice(middle, 0, newGuestInMiddle)
+ console.log(guestlist);
+
+ let newGuestAtEnd: String = "Najma Karim";
+ guestlist.push(newGuestAtEnd);
+console.log(guestlist);
+
+for(let guest of guestlist){
+    console.log(`Hello ${guest}, you are invited at the dinner tomorrow. Please come and bless us with your presence.`)
+};
+
+
+
