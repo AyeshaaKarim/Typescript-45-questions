36d908a43928471a9c8c9da0e1268621a30e8d10
AyeshaKarim <139690499+AyeshaaKarim@users.noreply.github.com>
2023-09-23 22:45:47 +0500

    Add files via upload

---
+++ Question-23/index.js
@@ -0,0 +1,40 @@
+"use strict";
+/* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru') */
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+console.log("Is car === 'Cultus'? I predict False.");
+console.log(car === 'Cultus');
+console.log("Is car === 'Toyota'? I predict False.");
+console.log(car === 'Toyota');
+console.log("Is car !='Subaru'? I predict True.");
+console.log(car != 'Subaru');
+console.log("Is car!= 'Civic'? I predict True.");
+console.log(car != 'Civic');
+let number = 50;
+console.log("Is number == 50? I predict True.");
+console.log(number == 50);
+console.log("Is number < 50? I predict False.");
+console.log(number < 50);
+console.log("Is number <= 50? I predict True.");
+console.log(number <= 50);
+console.log("Is number > 50? I predict False.");
+console.log(number > 50);
+console.log("Is number >= 50? I predict True.");
+console.log(number >= 50);
+console.log("Is number === 50? I predict True.");
+console.log(number === 50);
+console.log("Is number!== 50? I predict True.");
+console.log(number !== 50);
+console.log("Is number === 49? I predict False.");
+console.log(number === 49);
+console.log("Is number!== 49? I predict True.");
+console.log(number !== 49);
+console.log("Is number === 51? I predict False.");
+console.log(number === 51);
