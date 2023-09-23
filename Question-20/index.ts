569c7cf1d37baf6bd702b6a04f5cc7cbccdeeccf
AyeshaKarim <139690499+AyeshaaKarim@users.noreply.github.com>
2023-09-23 22:39:38 +0500

    Add files via upload

---
+++ Question-20/index.ts
@@ -0,0 +1,11 @@
+/* Think of something you could store in a array.
+ For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. 
+ Write a program that creates a list containing these items. */
+
+ const languages: String[]= ["English", "Urdu", "Punjabi", "Pushto"];
+ console.log("List of Languages:")
+
+ for (let language of languages){
+    console.log(language)
+ };
+ 
