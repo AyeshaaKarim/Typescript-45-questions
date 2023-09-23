9d32470277a39120df0122a036775153fc73be07
AyeshaKarim <139690499+AyeshaaKarim@users.noreply.github.com>
2023-09-23 22:19:23 +0500

    Add files via upload

---
+++ Question-5/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Kofi Annan";
+let message = ' once said, "There is no tool for development more effective than the empowerment of women."';
+console.log(famous_person + message);
