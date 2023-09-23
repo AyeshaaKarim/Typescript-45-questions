50024f48b962c18fa60c97053efff8234883ae3d
AyeshaKarim <139690499+AyeshaaKarim@users.noreply.github.com>
2023-09-23 22:18:16 +0500

    Add files via upload

---
+++ Question-4/index.ts
@@ -0,0 +1,3 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
+console.log('"Kofi Annan once said, "There is no tool for development more effective than the empowerment of women."');
